d39d1eab6fa004a87b2e4348b51c44d444264a7c
Vivek Dutta Mishra <vivek@conceptarchitect.in>
2023-09-20 16:20:29 +0530

    day#1 update#1

---
+++ basic-demos/programs/02_print_statement.py
@@ -0,0 +1,5 @@
+print(1,2,3,4)
+print('hello','world','python')
+
+print(1,2,3,4,5, end='\t', sep='-->')
+print(6,7,8, sep="|")
---
+++ basic-demos/programs/04_comments.py
@@ -0,0 +1,21 @@
+# This is the official comment syntax in python
+
+'I am a simple string. but I am not referred anywhere'
+'I will exist but not programmetically usable'
+'You may consider me as a comment'
+'You can also consider next line of code as comment'
+
+'common python operators'
+2+3
+10-2
+10*2
+10/2
+10//2
+
+'''
+I am a trippled qutoed string
+But people often consider me as comment
+Whilch I am certianly NOT.
+'''
+
+print('Hello World')
